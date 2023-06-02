d6e42522b1a7da6b576906f15caa21522340d5eb
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-02 05:08:32 +0000

    finish 3-5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  
+  protect_from_forgery with: :exception#クロスサイトリクエストフォージュエリ(CSRF)への対応策のコード(セキュリティ)
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #configure_permitted_parametersというメソッドはdevise_controllerを使う時しか処理しない
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+    #nameカラムを保存できるようにする記述はサインアップやアカウントをアップデートするときだけにしか必要がない。
 end
