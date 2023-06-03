2c71bbf7947cb2d6e8830b95627903051c72cb2f
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-03 02:10:07 +0000

    finish7-1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :set_post, only: %i[show destroy]
+
   def new
     @post = Post.new# id: nil, caption: nil, user_id: nil, created_at: nil, updated_at: nilのインスタンスを作っている
     @post.photos.build# Railsの慣習でモデルを関連付けしたときにbuildを使う。
   end
-  
+
   def create
     @post = Post.new(post_params)#post_paramsを引数にしてnewメソッドでインスタンスを作成している
     if @post.photos.present?#投稿の写真が存在するか確認している。present?はnilまたは空のオブエクトでなければtrueを返します。
@@ -17,7 +18,7 @@ class PostsController < ApplicationController
       flash[:alert] = "投稿に失敗しました"
     end
   end
-  
+
   def index
     @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
     #limitメソッドは取り出すレコード数の上限を指定する。
@@ -26,6 +27,22 @@ class PostsController < ApplicationController
     #.includes(:photos, :user)でN+1問題が解決する。
   end
 
+  def show
+    #@post = Post.find_by(id: params[:id])
+    #受け取ったHTTPリクエストからidを判別し指定のレコード1つを@postに代入している。
+  end
+  
+  def destroy
+    #@post = Post.find_by(id: params[:id])#受け取ったHTTPリクエストからidを判別し、指定の投稿1つを@postに代入している。
+    if @post.user == current_user        #投稿したユーザーと現在サインインしているユーザーが等しければ、真を返す条件式。
+      flash[:notice] = "投稿が削除されました" if @post.destroy
+      #@post.destroyが真だったら、「投稿が削除されました」というフラッシュメッセージを表示
+    else
+      flash[:alert] = "投稿の削除に失敗しました"
+    end
+    redirect_to root_path
+  end
+  
   private
     def post_params
       params.require(:post).permit(:caption, photos_attributes: [:image]).merge(user_id: current_user.id)
@@ -33,6 +50,10 @@ class PostsController < ApplicationController
       #requireで受け取る値のキーを設定します。
       #permitで変更を加えられるキーを指定する。今回はcaptionキーとimageキーを指定している。
       #mergeメソッドは２つの8種を統合するメソッド。今回は誰が投稿したのかという情報が必要なためuser_idの情報を統合している。
+    end
+    
+    def set_post
+      @post = Post.find_by(id: params[:id])
     end
     
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
+  has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+  #いいねを取得する時、作られた逆順で取得=>新しいいいね順で取得
   accepts_nested_attributes_for :photos#使用しないほうがいいらしい
 end
