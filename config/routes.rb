f3a606ae5a94da4f77c123340e8298a8faed99e2
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-03 05:47:08 +0000

    コメント機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :posts, only: %i[new create index show destroy] do
     resources :photos, only: %i[create]#postsにphotosが紐づいている
     resources :likes, only: %i[create destroy]#postsにlikesが紐づいている
+    resources :comments, only: %i[create destroy]#postsにcommentsが紐づいている
   end
-
 
 end
